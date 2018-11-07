d2f8ea945b9007d982a8dd898902c68dc1d6792e
Brayan Andres Restrepo Galeano <brayan2293@gmail.com>
2018-11-06 20:00:08 -0500

    crear factura list item

--- src/main/java/com/udemy/models/dao/IProductoDao.java
+++ src/main/java/com/udemy/models/dao/IProductoDao.java
@@ -11,4 +11,7 @@ public interface IProductoDao extends CrudRepository<Producto, Long>{
 
 	@Query("select p from Producto p where p.nombre like %?1%")
 	public List<Producto> findByNombre(String term);
+	
+	// es lo mismo que el anterior metodo pero es automatico ver documentacion es interesante video 79 
+	public List<Producto> findByNombreLikeIgnoreCase(String term);
 }
